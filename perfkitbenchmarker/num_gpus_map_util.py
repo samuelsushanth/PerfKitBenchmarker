a1b007817ff5de002e33f9768b2ae92cc446a76b
Gareth Ferneyhough <ferneyhough@google.com>
2017-03-02 15:18:09 -0800

    Add SHOC benchmark and package

---
+++ perfkitbenchmarker/num_gpus_map_util.py
@@ -0,0 +1,15 @@
+gpus_per_vm = {
+    'n1-standard-4-k80x1': 1,
+    'n1-standard-8-k80x1': 1,
+    'n1-standard-8-k80x2': 2,
+    'n1-standard-16-k80x2': 2,
+    'n1-standard-16-k80x4': 4,
+    'n1-standard-32-k80x4': 4,
+    'n1-standard-32-k80x8': 8,
+    'p2.xlarge': 1,
+    'p2.8xlarge': 8,
+    'p2.16xlarge': 16,
+    'Standard_NC6': 1,
+    'Standard_NC12': 2,
+    'Standard_NC24': 4
+}
